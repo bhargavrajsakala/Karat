8c03566ea5d15d23b6ced15e37653593501f3cae
bhargavrajsakala <bobbyvizag8899@gmail.com>
2026-01-12 12:43:00 +0530

    karat 9

--- As5Case.cs
+++ As5Case.cs
@@ -1,54 +1,126 @@
-using System; 
- using System.Collections.Generic; 
- using System.Linq; 
-
-  
-
-public class OrderItem 
- { 
-     public string Name { get; set; } 
-     public double Price { get; set; } 
-     public int Quantity { get; set; } 
- } 
+/*
+We are building a word processor and we would like to implement a "word-wrap" functionality.
  
- public class Order 
- { 
-     public List<OrderItem> Items { get; set; } = new List<OrderItem>(); 
-     public double GetTotalPrice() 
-     {
-        double subtotal = Items.Sum(item => item.Price * item.Quantity); 
-         if (subtotal > 100) 
-         { 
-             return subtotal * 0.90;  
-         } 
-         return subtotal; 
-     } 
- } 
- public class EmailService 
- { 
-     public void SendConfirmation(string emailAddress) 
-     { 
-         Console.WriteLine($"Sending order confirmation email to: {emailAddress}"); 
-         Console.WriteLine("Email content: Your order has been successfully placed!"); 
+Given a list of words followed by a maximum number of characters in a line, return a collection of strings where each string element represents a line that contains as many words as possible, with the words in each line being concatenated with a single '-' (representing a space, but easier to see for testing). The length of each string must not exceed the maximum character length per line.
  
-     } 
- } 
+Your function should take in the maximum characters per line and return a data structure representing all lines in the indicated max length.
+ 
+Examples:
+ 
+words1 = [ "The", "day", "began", "as", "still", "as", "the",
+          "night", "abruptly", "lighted", "with", "brilliant",
+          "flame" ]
+ 
+wrapLines(words1, 13) "wrap words1 to line length 13" =>
+ 
+  [ "The-day-began",
+    "as-still-as",
+    "the-night",
+    "abruptly",
+    "lighted-with",
+    "brilliant",
+    "flame" ]
+ 
+wrapLines(words1, 12) "wrap words1 to line length 12" =>
+ 
+  [ "The-day",
+    "began-as",
+    "still-as-the",
+    "night",
+    "abruptly",
+    "lighted-with",
+    "brilliant",
+    "flame" ]    
+ 
+wrapLines(words1, 20) "wrap words1 to line length 20" =>
+ 
+  [ "The-day-began-as",
+    "still-as-the-night",
+    "abruptly-lighted",
+    "with-brilliant-flame" ]
+ 
+words2 = [ "Hello" ]
+ 
+wrapLines(words2, 5) "wrap words2 to line length 5" =>
+ 
+  [ "Hello" ]
+ 
+wrapLines(words2, 30) "wrap words2 to line length 30" =>
+ 
+  [ "Hello" ]  
+ 
+words3 = [ "Hello", "Hello" ]
+ 
+wrapLines(words3, 5) "wrap words3 to line length 5" =>
+ 
+  [ "Hello",
+  "Hello" ]
+ 
+words4 = ["Well", "Hello", "world" ]
+ 
+wrapLines(words4, 5) "wrap words4 to line length 5" =>
+ 
+  [ "Well",
+  "Hello",
+  "world" ]
+ 
+words5 = ["Hello", "HelloWorld", "Hello", "Hello"]
+ 
+wrapLines(words5, 20) "wrap words 5 to line length 20 =>
+ 
+  [ "Hello-HelloWorld",
+    "Hello-Hello" ]
+ 
+words6 = [ "a", "b", "c", "d" ]
+wrapLines(words6, 20) "wrap words 6 to line length 20 =>
+ 
+  [ "a-b-c-d" ]
+ 
+wrapLines(words6, 4) "wrap words 6 to line length 4 =>
+ 
+  [ "a-b",
+    "c-d" ]
+ 
+wrapLines(words6, 1) "wrap words 6 to line length 1 =>
+ 
+  [ "a",
+    "b",
+    "c",
+    "d" ]
+ 
+All Test Cases:
+          words,  max line length
+wrapLines(words1, 13)
+wrapLines(words1, 12)
+wrapLines(words1, 20)
+wrapLines(words2, 5)
+wrapLines(words2, 30)
+wrapLines(words3, 5)
+wrapLines(words4, 5)
+wrapLines(words5, 20)
+wrapLines(words6, 20)
+wrapLines(words6, 4)
+wrapLines(words6, 1)
+ 
+n = number of words OR total characters
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program 
+{
+  public List<string> wrapLines(string[] words, int length) 
+  {
+    // TODO: Implement the wrap logic here
+    return new List<string>();
+  }
 
-  
-
-public class OrderProcessor 
- { 
-     private readonly EmailService emailService; 
-     public OrderProcessor(EmailService service) 
-     { 
-         emailService = service; 
-     } 
- 
-     public void ProcessOrder(Order order, string customerEmail) 
-     { 
-         double total = order.GetTotalPrice();  
-         Console.WriteLine($"Order total (after discount if applicable): ${total:F2}");  
-         emailService.SendConfirmation(customerEmail); 
-     }
- }
- 
+  static void Main(String[] args) 
+  {
+    String[] words1 = new String[] {"The","day","began","as","still","as","the","night","abruptly","lighted","with","brilliant","flame"};
+    Program p = new Program();
+    var result = p.wrapLines(words1, 13);
+    foreach(var line in result)
+      Console.WriteLine(line);
+  }
+}
